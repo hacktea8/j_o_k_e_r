ad7f05e3032b80b3f4cf7e2adbae9dd3c0192ea8
hacktea8 <admin@hacktea8.com>
2014-08-17 13:47:20 +0800

    update

--- application/controllers/maindex.php
+++ application/controllers/maindex.php
@@ -50,7 +50,7 @@ class Maindex extends Usrbase {
     $config['cur_tag_close'] = '</span>';
     $config['suffix'] = '.html';
     $config['use_page_numbers'] = TRUE;
-    $config['num_links'] = 5;
+    $config['num_links'] = 4;
     $config['cur_page'] = $page;
 
     $this->pagination->initialize($config);
@@ -227,7 +227,7 @@ class Maindex extends Usrbase {
     $pageSize = 12;
     if($q){
       $this->load->library('yunsearchapi');
-      $opt = array('query'=>$q,'start'=>$page,'hits'=>$pageSize);
+      $opt = array('query'=>$q,'start'=>$page*$pageSize,'hits'=>$pageSize);
       $this->yunsearchapi->search($list,$opt);
       $hotKeywords = $this->yunsearchapi->getTopQuery($num=8,$days=30);
       //var_dump($hotKeywords);exit;
@@ -241,6 +241,7 @@ var_dump($q);
 var_dump($hotKeywords);
 var_dump($list);exit;
 /**/
+    $page++;
     $hot_search = array();
     $recommen_topic = array();
     $recommen_topic[1] = array();
@@ -260,11 +261,16 @@ var_dump($list);exit;
     $config['cur_tag_close'] = '</span>';
     $config['suffix'] = '.shtml';
     $config['use_page_numbers'] = TRUE;
-    $config['num_links'] = 5;
+    $config['num_links'] = 4;
     $config['cur_page'] = $page;
     $this->pagination->initialize($config);
     $page_string = $this->pagination->create_links();
-    $this->assign(array('searchlist'=>$list['result'],'kw'=>$q,'q'=>$q,'page_string'=>$page_string,'hot_search'=>$hot_search,'recommen_topic'=>$recommen_topic,'hot_topic'=>$hot_topic)); 
+    $seo_title = sprintf('正在搜索%s第%d页',$q,$page);
+    $this->assign(array('searchlist'=>$list['result'],'kw'=>$q,'q'=>$q
+    ,'page_string'=>$page_string,'hot_search'=>$hot_search
+    ,'recommen_topic'=>$recommen_topic,'hot_topic'=>$hot_topic
+    ,'seo_title'=>$seo_title
+    ));
     $this->load->view('index_search',$this->viewData);
   }
   public function show404($goto = ''){
